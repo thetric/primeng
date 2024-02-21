3a64e65338b8db4e431a49377f276e412853526f
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2024-02-21 13:17:03 +0300

    add hooks

--- src/app/showcase/doc/panel/basicdoc.ts
+++ src/app/showcase/doc/panel/basicdoc.ts
@@ -9,7 +9,7 @@ import { PrimeNGConfig } from 'primeng/api';
             <p>A simple Panel is created with a <i>header</i> property along with the content as children.</p>
         </app-docsectiontext>
         <div class="card flex justify-content-center">
-            <p-panel header="Header" [pt]="{ root: myMethod.bind(this) }" [toggleable]="true">
+            <p-panel header="Header" [toggleable]="true">
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
                     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -32,8 +32,41 @@ export class BasicDoc {
 
     hooks() {
         return {
+            onChanges: () => {
+                // do something
+                // console.log('ONCHANGES from basicdoc');
+            },
+            onInit: () => {
+                // do something
+                // console.log('ONINIT from basicdoc');
+            },
+            afterContentChecked: () => {
+                // do something
+                // console.log('AFTERCONTENTCHECKED from basicdoc');
+            },
             afterViewInit: () => {
                 // do something
+                // console.log('AFTERVIEWINIT from basicdoc');
+            },
+            afterViewChecked: () => {
+                // do something
+                // console.log('AFTERVIEWCHECKED from basicdoc');
+            },
+            afterRender: () => {
+                // do something
+                // console.log('AFTERRENDER from basicdoc');
+            },
+            afterNextRender: () => {
+                // do something
+                // console.log('AFTERNEXTRENDER from basicdoc');
+            },
+            afterContentInit: () => {
+                // do something
+                // console.log('AFTERCONTENTINIT from basicdoc');
+            },
+            onDestroy: () => {
+                // do something
+                // console.log('ONDESTROY from basicdoc');
             }
         };
     }
